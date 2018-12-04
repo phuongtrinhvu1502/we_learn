f95e7e47ca21caf5a6e5369e057efa024a716185
xuanhong95 <hong.pham@hacvntech.com>
2018-12-04 22:13:18 +0700

    update BE article topic content

--- src/com/we_learn/dao/ArticleTopicContentDao.java
+++ src/com/we_learn/dao/ArticleTopicContentDao.java
@@ -3,8 +3,12 @@ package com.we_learn.dao;
 import org.json.simple.JSONObject;
 
 public interface ArticleTopicContentDao {
-	JSONObject update(String param, String user_id);
 	JSONObject insert(String param, String user_id);
-	JSONObject delete(String param, String user_id);
-	JSONObject remove(String param, String user_id);
+	JSONObject update(String param, String user_id);
+	public JSONObject delete(String article, int user_id);
+	public JSONObject remove(String article, int user_id);
+	public JSONObject restore(String article, int user_id);
+	public JSONObject getArticleById(String atc_id);
+	public JSONObject viewArticleById(String atc_id);
+	JSONObject getTopicByPage(String param);
 }
--- src/com/we_learn/domain/ArticleTopicContentController.java
+++ src/com/we_learn/domain/ArticleTopicContentController.java
@@ -1,9 +1,13 @@
 package com.we_learn.domain;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
@@ -16,9 +20,11 @@ import org.springframework.web.context.ContextLoader;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.we_learn.common.VerifyToken;
+import com.we_learn.dao.ArticleDao;
+import com.we_learn.dao.ArticleDaoImp;
 import com.we_learn.dao.ArticleTopicContentDao;
 import com.we_learn.dao.ArticleTopicContentDaoImp;
-@Path("/artice-topic-content")
+@Path("/atc")
 public class ArticleTopicContentController extends VerifyToken{
 
 	public ArticleTopicContentController(@HeaderParam("Authorization") String token) {
@@ -52,4 +58,84 @@ public class ArticleTopicContentController extends VerifyToken{
 		JSONObject result = articleTopicContentDao.update(param, this.userId);
 		return Response.status(200).entity(result.toString()).build();
 	}
+	@PUT
+	@Path("remove")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response remove(@HeaderParam("Authorization") String token, String article) {
+		if (!this.isLogined)
+			return Response.status(200).entity(this.notFoundUser().toString()).build();
+		ArticleTopicContentDao articleTopicContentDao = (ArticleTopicContentDaoImp) this.appContext.getBean("articleTopicContentDao");
+		JSONObject result = articleTopicContentDao.remove(article, Integer.parseInt(this.userId));
+		return Response.status(200).entity(result.toString()).build();
+	}
+
+	@PUT
+	@Path("restore")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response restore(@HeaderParam("Authorization") String token, String article) {
+		if (!this.isLogined)
+			return Response.status(200).entity(this.notFoundUser().toString()).build();
+		ArticleTopicContentDao articleTopicContentDao = (ArticleTopicContentDaoImp) this.appContext.getBean("articleTopicContentDao");
+		JSONObject result = articleTopicContentDao.restore(article, Integer.parseInt(this.userId));
+		return Response.status(200).entity(result.toString()).build();
+	}
+
+	@DELETE
+	@Path("delete")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response delete(@HeaderParam("Authorization") String token, String article) {
+		if (!this.isLogined)
+			return Response.status(200).entity(this.notFoundUser().toString()).build();
+		ArticleTopicContentDao articleTopicContentDao = (ArticleTopicContentDaoImp) this.appContext.getBean("articleTopicContentDao");
+		JSONObject result = articleTopicContentDao.delete(article, Integer.parseInt(this.userId));
+		return Response.status(200).entity(result.toString()).build();
+	}
+
+	@POST
+	@Path("get-article-by-page")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getTopicByPage(@HeaderParam("Authorization") String token, String param) {
+		if (!this.isLogined)
+			return Response.status(200).entity(this.notFoundUser().toString()).build();
+		ArticleTopicContentDao articleTopicContentDao = (ArticleTopicContentDaoImp) this.appContext.getBean("articleTopicContentDao");
+		JSONObject result = articleTopicContentDao.getTopicByPage(param);
+		return Response.status(200).entity(result.toString()).build();
+	}
+
+	@GET
+	@Path("get-article-by-id")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getArticleById(@HeaderParam("Authorization") String token, @Context HttpServletRequest request) {
+		if (!this.isLogined)
+			return Response.status(200).entity(this.notFoundUser().toString()).build();
+		ArticleTopicContentDao articleTopicContentDao = (ArticleTopicContentDaoImp) this.appContext.getBean("articleTopicContentDao");
+		JSONObject result = articleTopicContentDao.getArticleById(request.getParameter("atc_id"));
+		return Response.status(200).entity(result.toString()).build();
+	}
+
+	@GET
+	@Path("view-article-by-id")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response viewArticleById(@HeaderParam("Authorization") String token, @Context HttpServletRequest request) {
+		ArticleTopicContentDao articleTopicContentDao = (ArticleTopicContentDaoImp) this.appContext.getBean("articleTopicContentDao");
+		JSONObject result = articleTopicContentDao.viewArticleById(request.getParameter("atc_id"));
+		return Response.status(200).entity(result.toString()).build();
+	}
+//	@POST
+//	@Path("get-article-by-type")
+//	@Consumes(MediaType.APPLICATION_JSON)
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Response getArticleByType(@HeaderParam("Authorization") String token, String param) {
+//		if (!this.isLogined)
+//			return Response.status(200).entity(this.notFoundUser().toString()).build();
+//		ArticleDao articleDao = (ArticleDaoImp) this.appContext.getBean("articleDao");
+//		JSONObject result = articleDao.getListArticleByType(param);
+//		return Response.status(200).entity(result.toString()).build();
+//	}
 }
